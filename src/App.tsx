cadc16260b28611cb73b10f154be918c96b9ed27
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2022-08-27 23:32:59 +1000

    ReactAppFakeStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios, { Axios, AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Product } from './components/product'
+//import {products} from './data/products'
+import { IProduct } from './models';
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  async function fetchProducts() {
+    try {
+      setError('')
+      setLoading(true)
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
+    } catch (e: unknown) {
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
+      {products.map(product => <Product product={product} key={product.id} />)}
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/product.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react"
+import { products } from "../data/products"
+import {IProduct} from '../models'
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product({product} : ProductProps){
+const [details, setDetails] = React.useState(false)
+
+const btnBgClassName = details ? 'bg-blue-400' : 'bg-yellow-400' 
+
+const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
+    return(
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src={product.image} className = "w-16" alt={product.title}></img>
+            <p>{product.title}</p>
+            <span className="font-bold">{product.price}</span>
+            <button
+            className = {btnClasses.join(' ')}
+            onClick={() => setDetails(prev => !prev)} 
+            >
+                {details ? 'Hide Details':'Show Detailes'}
+            </button>
+            
+            {details && <div>
+                <p>{product.description}</p>
+                <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+            </div>}
+        </div>
+    )
+}
